f3824b89907e243946aaf3a4cdc3b0a7bfbae5f6
SnehaUmrit <hcysu1@nottingham.edu.my>
2021-02-08 14:45:12 +0400

    Atomic time up to millisecond/Offset calculated and displayed

--- app/src/main/java/com/group10b/blueka_sync/MainActivity.java
+++ app/src/main/java/com/group10b/blueka_sync/MainActivity.java
@@ -1,5 +1,8 @@
 package com.group10b.blueka_sync;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,16 +12,22 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.net.InetAddress;
 import java.util.Date;
+import java.util.TimeZone;
+
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TIME_SERVER = "ntp.xs4all.nl";
+    public static final String SERVER_NAME = "pool.ntp.org";
     private static final String TAG = "";
+    final SntpClient sntpClient = new SntpClient();
+    //final NTPUDPClient client = new NTPUDPClient();
+
 
 
     @Override
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                     while (!isInterrupted()) {
                         Thread.sleep(10);
                         runOnUiThread(new Runnable() {
+                            @SuppressLint("StaticFieldLeak")
                             @Override
                             public void run() {
                                 /**
@@ -48,16 +58,45 @@ public class MainActivity extends AppCompatActivity {
                                 /**
                                  * Obtaining atomic time from internet via NTP and displaying on screen
                                  */
-                                /*TextView atomicTime = (TextView) findViewById(R.id.atomic);
-                                long at = 0;
-                                try {
-                                    at = getCurrentNetworkTime();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                                SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss.SS");
-                                String atomicString = sdf2.format(at);
-                                atomicTime.setText(atomicString);*/
+
+                               /*
+                                new GetNTPAsynctask().execute();
+                                if (client.requestTime("pool.ntp.org",10)){
+                                    long at = client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
+                                    SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss.SS");
+                                    String atomicString = sdf2.format(at);
+                                    atomicTime.setText(atomicString);
+                                }*/
+
+                                TextView atomicTime = (TextView) findViewById(R.id.atomic);
+                                TextView offsetView = (TextView) findViewById(R.id.offsetId);
+                                new AsyncTask<Void,Integer,Boolean>(){
+                                    @Override
+                                    protected Boolean doInBackground(Void... params) {
+                                        return sntpClient.requestTime("pool.ntp.org",3000);
+                                    }
+                                    @Override
+                                    protected void onPostExecute(Boolean result) {
+                                        if(result){
+                                            long at = sntpClient.getNtpTime();
+                                            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SS");
+                                            String atomicString = formatter.format(at);
+                                            atomicTime.setText(atomicString);
+
+                                            System.out.println("Atomic Time: " + sntpClient.getNtpTime());
+                                            System.out.println("System Time: " + System.currentTimeMillis());
+                                            long offset = System.currentTimeMillis() - sntpClient.getNtpTime() ;
+                                            System.out.println("Offset: " + offset);
+                                            SimpleDateFormat offsetFormat = new SimpleDateFormat("ss.S");
+                                            String offsetString = offsetFormat.format(offset);
+                                            offsetView.setText(offsetString);
+
+                                        }
+                                    }
+                                }.execute();
+
+
+
 
 
                             }
@@ -68,8 +107,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         t.start();
-
-
 
 
 
@@ -93,3 +130,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
