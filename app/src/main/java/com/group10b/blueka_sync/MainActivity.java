4dd662d396bbf502b314ee380d5e4e4ff0d1fc9e
SnehaUmrit <hcysu1@nottingham.edu.my>
2021-02-08 11:38:39 +0400

    Added library (apache-commons-net.jar)

--- app/src/main/java/com/group10b/blueka_sync/MainActivity.java
+++ app/src/main/java/com/group10b/blueka_sync/MainActivity.java
@@ -1,17 +1,32 @@
 package com.group10b.blueka_sync;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
+import java.net.InetAddress;
+import java.util.Date;
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TIME_SERVER = "ntp.xs4all.nl";
+    private static final String TAG = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Thread t = new Thread() {
             @Override
             public void run() {
@@ -21,12 +36,30 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                TextView tdate = (TextView) findViewById(R.id.system);
-                                long date = System.currentTimeMillis();
-                                //SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh-mm-ss a");
+                                /**
+                                 * Obtaining system time and displaying on screen
+                                 */
+                               TextView systemTime = (TextView) findViewById(R.id.system);
+                                long st = System.currentTimeMillis();
                                 SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SS");
-                                String dateString = sdf.format(date);
-                                tdate.setText(dateString);
+                                String systemString = sdf.format(st);
+                                systemTime.setText(systemString);
+
+                                /**
+                                 * Obtaining atomic time and displaying on screen
+                                 */
+                                /*TextView atomicTime = (TextView) findViewById(R.id.atomic);
+                                long at = 0;
+                                try {
+                                    at = getCurrentNetworkTime();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss.SS");
+                                String atomicString = sdf2.format(at);
+                                atomicTime.setText(atomicString);*/
+
+
                             }
                         });
                     }
@@ -35,5 +68,28 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         t.start();
+
+
+
+
+
     }
+
+
+
+  /*public static long getCurrentNetworkTime() throws IOException {
+        NTPUDPClient timeClient = new NTPUDPClient();
+        InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+        TimeInfo timeInfo = timeClient.getTime(inetAddress);
+        //long returnTime = timeInfo.getReturnTime();   //local device time
+        long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();   //server time
+
+        Date time = new Date(returnTime);
+        Log.d(TAG, "Time from " + TIME_SERVER + ": " + time);
+
+        return returnTime;
+    }*/
+
+
+
 }
